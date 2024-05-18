0235d03091f8dcefbb68fe7263cfddae56cbd6cd
harshgupta2002 <gup.harsh.2002@gmail.com>
2024-05-18 20:37:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+import Game from './components/Game';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+        <h1 style={{margin: "4rem", marginTop:"-2rem"}}>Tic-Tac-Toe</h1>
+            <Game />
+        </div>
+    );
 }
 
 export default App;
